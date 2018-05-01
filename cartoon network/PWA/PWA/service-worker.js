a578e00678621d1e14246f7b09b710f9393e084d
WayneKuo <aasdfangela@gmail.com>
2018-05-02 02:34:25 +0800

    PWA

--- cartoon network/PWA/PWA/service-worker.js
+++ cartoon network/PWA/PWA/service-worker.js
@@ -1,6 +1,6 @@
-<script language="JavaScript">
+﻿<script language="JavaScript">
 ‘use strict’;
-importScripts(『sw-toolbox.js』); 
+importScripts('sw-toolbox.js'); 
 toolbox.precache([「index.html」,」style/style.css」]); 
 toolbox.router.get(『/images/*』, toolbox.cacheFirst); 
 toolbox.router.get(『/*』, toolbox.networkFirst, 
