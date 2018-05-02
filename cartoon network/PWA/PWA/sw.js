3c127ae238053cc0e47b65065c4e3341ac2ddaf1
WayneKuo <aasdfangela@gmail.com>
2018-05-02 10:25:48 +0800

    PWA

--- cartoon network/PWA/PWA/sw.js
+++ cartoon network/PWA/PWA/sw.js
@@ -1,12 +1,26 @@
-<script language="JavaScript">
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('./sw.js')
-    .then(reg => {
-      // registration worked
-      console.log('[Service Worker] Registration succeeded. Scope is ' + reg.scope);
-    }).catch(error => {
-      // registration failed
-      console.log('[Service Worker] Registration failed with ' + error);
-    });
-}
-</script>
+const cacheVersion = 'v1';
+const filesToCache = [
+  'index.html',
+  'register_sw.js',
+  
+];
+self.addEventListener('install', event => {
+  console.log('[ServiceWorker] Install');
+  event.waitUntil(
+    caches.open(cacheVersion)
+    .then(cache => {
+      console.log('[ServiceWorker] Caching app shell');
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+self.addEventListener('activate', event => {
+  console.log('[ServiceWorker] Activate');
+});
+self.addEventListener('fetch', event => {
+  console.log('[ServiceWorker] fetch', event.request);
+  event.respondWith(
+    caches.match(event.request)
+    .then(response => response || fetch(event.request))
+  );
+});
